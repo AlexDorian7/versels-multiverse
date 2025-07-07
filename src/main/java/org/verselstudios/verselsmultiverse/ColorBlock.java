d1d12b95de0d266f19771df262e0ab11b4c7e00f
Alex <alexdollar3@gmail.com>
2025-07-07 13:01:24 -0500

    Updated readme

---
+++ src/main/java/org/verselstudios/verselsmultiverse/ColorBlock.java
@@ -0,0 +1,9 @@
+package org.verselstudios.verselsmultiverse;
+
+import net.minecraft.world.level.block.Block;
+
+public class ColorBlock extends Block {
+    public ColorBlock(Properties properties) {
+        super(properties);
+    }
+}
